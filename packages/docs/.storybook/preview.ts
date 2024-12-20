0e92d29eac677c31e264d1c31e23ba9fb79f60b1
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-20 12:33:07 -0300

    adicionando documentacoes de arquivos e ajustes de estrutura

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,12 +1,26 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: "dark",
+      values: [
+        { name: "light", value: "#ffffff" },
+        { name: "dark", value: "#1B1C1D" },
+      ],
+    },
+    darkMode: {
+      current: "dark",
     },
   },
 };
