ea70f0c84a65f84e6af0a956604185057b250342
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-20 15:23:32 -0300

    adicionando novas docs para tokens

--- packages/docs/src/stories/Button.stories.ts
+++ packages/docs/src/stories/Button.stories.ts
@@ -2,6 +2,7 @@ import { Button, ButtonProps } from "@fialho-ui/react";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<ButtonProps> = {
+  title: "Form/Button",
   component: Button,
   args: {
     children: "Enviar",
